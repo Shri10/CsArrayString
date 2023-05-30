77a11ddf69eb83391072d0012193ed23e94f0856
Shri10 <pethe.shriraj@gmail.com>
2023-05-30 23:55:51 +0530

    [Shri10] Created Program 2

---
+++ CsArrayString/PrintUnique.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsArrayString
+{
+    internal class PrintUnique
+    {
+        public static void PrintUniqOnly(int[] array) 
+        {
+            Console.WriteLine("Unique Numbers");
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (i != j && array[j] == array[i])
+                    { 
+                        isUnique = false; 
+                        break; 
+                    }
+                }
+                if (isUnique)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
--- CsArrayString/Program.cs
+++ CsArrayString/Program.cs
@@ -11,6 +11,7 @@
             {
                 Console.WriteLine("\nPlease choose from below options:");
                 Console.WriteLine("1. Count duplicate elements in an array");
+                Console.WriteLine("2. Print unique elements in an array");
                 Console.WriteLine("0. Exit");
 
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +23,13 @@
                         //Add option to chose from predefined array, or define your custom array.
                         DuplicateElements.DuplicateCount(arr);
                         break;
+                    case 2:
+
+                        //Option 2
+                        //Add option to chose from predefined array, or define your custom array.
+                        PrintUnique.PrintUniqOnly(arr);
+                        break;
+
                     case 0:
                         return;
                     default:
