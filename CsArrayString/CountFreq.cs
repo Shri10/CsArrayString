b0752a4b100cc601826694d9a2f514c2c395be86
Shri10 <pethe.shriraj@gmail.com>
2023-05-30 23:57:40 +0530

    [Shri10] Created Program 3

---
+++ CsArrayString/CountFreq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsArrayString
+{
+    internal class CountFreq
+    {
+        public static void Freq(int[] array)
+        {
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            foreach (int num in array)
+            {
+                if (frequency.ContainsKey(num))
+                {
+                    frequency[num]++;
+                }
+                else
+                {
+                    frequency[num] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> item in frequency)
+            {
+                Console.WriteLine($"Element {item.Key} occurs {item.Value} times");
+            }
+        }
+    }
+}
--- CsArrayString/Program.cs
+++ CsArrayString/Program.cs
@@ -12,6 +12,7 @@
                 Console.WriteLine("\nPlease choose from below options:");
                 Console.WriteLine("1. Count duplicate elements in an array");
                 Console.WriteLine("2. Print unique elements in an array");
+                Console.WriteLine("3. Count frequency of each element in an array");
                 Console.WriteLine("0. Exit");
 
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -28,6 +29,12 @@
                         //Option 2
                         //Add option to chose from predefined array, or define your custom array.
                         PrintUnique.PrintUniqOnly(arr);
+                        break;
+                    case 3:
+
+                        //Option 3
+                        //Add option to chose from predefined array, or define your custom array.
+                        CountFreq.Freq(arr);
                         break;
 
                     case 0:
